9d1f9fd391b1b6a632d94a67047b55034428ab32
PerrySawatzky <sawatzky.perry@gmail.com>
2021-05-24 12:54:46 -0400

    Gotta love new that new repo smell

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rocket_Elevators_Csharp_Controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write more code about elevators");
+        }
+    }
+}
